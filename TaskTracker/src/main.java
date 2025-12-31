6722cdf3dc72bef559d3e15ed2aa5956b739f42f
CodeMorera <morerachristian@yahoo.com>
2025-12-31 11:42:00 -0500

    Add Maven project structure

---
+++ TaskTracker/src/main.java
@@ -0,0 +1,9 @@
+package src;
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("**************************");
+        System.out.println("Creating your Task Tracker");
+        System.out.println("**************************");
+    }
+}
